6c10e6c0f909779386f2725bc0d430a3d85353c1
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-04-02 15:10:21 +0900

    feat: Add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { lazy, Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const IndexPage = lazy(() => import('./pages/IndexPage'));
+const RoomPage = lazy(() => import('./pages/RoomPage'));
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Suspense fallback="Loading...">
+      <div>
+        <div>
+          <Link to="/">Home</Link>|<Link to="/room/abc">Room</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/room/:roomId" element={<RoomPage />} />
+        </Routes>
+      </div>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/RoomPage.tsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom';
+
+export default function RoomPage() {
+  const { roomId } = useParams<'roomId'>();
+  assertDefined(roomId, 'roomId');
+
+  return <>RoomPage: {roomId}</>;
+}
+
+function assertDefined<T>(
+  value: T | undefined,
+  name: string
+): asserts value is T {
+  if (value === undefined) {
+    throw new Error(`The ${name} is undefined`);
+  }
+}
