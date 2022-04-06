63ac6dd6de0f9198816b78388fd476acc9f31e78
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-04-07 08:31:11 +0900

    feat: 不要なリンクを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
 import { lazy, Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import { Link as RouterLink, Route, Routes } from 'react-router-dom';
-import { Box, Link } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 
 const IndexPage = lazy(() => import('./pages/IndexPage'));
 const RoomPage = lazy(() => import('./pages/RoomPage'));
 
 function App() {
   return (
-    <ErrorBoundary
-      fallbackRender={(error) => <div>Error: {error.error.message}</div>}
-    >
-      <Suspense fallback="Loading...">
-        <Box minH="100vh">
-          <div>
-            <Link as={RouterLink} to="/">
-              Home
-            </Link>
-            |
-            <Link as={RouterLink} to="/room/abc">
-              Room
-            </Link>
-          </div>
+    <Box minH="100vh">
+      <ErrorBoundary
+        fallbackRender={(error) => <div>Error: {error.error.message}</div>}
+      >
+        <Suspense fallback="Loading...">
           <Routes>
             <Route path="/" element={<IndexPage />} />
             <Route path="/room/:roomId" element={<RoomPage />} />
           </Routes>
-        </Box>
-      </Suspense>
-    </ErrorBoundary>
+        </Suspense>
+      </ErrorBoundary>
+    </Box>
   );
 }
 
