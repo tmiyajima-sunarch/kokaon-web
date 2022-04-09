da1aa777c6b3cbbcab13dd99a2863e1ad00b2393
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-04-09 09:44:17 +0900

    refactor: useAudio()を作成

--- src/pages/RoomPage/AudioList.tsx
+++ src/pages/RoomPage/AudioList.tsx
@@ -21,6 +21,30 @@ export default function AudioList({ room, audios }: AudioListProps) {
 }
 
 function AudioListItem({ room, audio }: { room: Room; audio: AudioData }) {
+  const { isRejected, onAllow, isPlaying, isPlayable, onPause, onPlay } =
+    useAudio(room, audio);
+
+  return (
+    <Flex justifyContent="space-between" alignItems="center" h="10">
+      <Box>{audio.name}</Box>
+      {isRejected ? (
+        <Button size="sm" onClick={onAllow}>
+          再生を許可する
+        </Button>
+      ) : (
+        <IconButton
+          size="sm"
+          icon={isPlaying ? <FaPause /> : <FaPlay />}
+          aria-label="再生"
+          isLoading={!isPlayable}
+          onClick={isPlaying ? onPause : onPlay}
+        />
+      )}
+    </Flex>
+  );
+}
+
+function useAudio(room: Room, audio: AudioData) {
   const [isPlayable, setPlayable] = useState(false);
   const [isRejected, setRejected] = useState(false);
   const [isPlaying, setPlaying] = useState(false);
@@ -81,7 +105,7 @@ function AudioListItem({ room, audio }: { room: Room; audio: AudioData }) {
     };
   }, [audio.id, audio.name, player, room]);
 
-  const allow = useCallback(async () => {
+  const onAllow = useCallback(async () => {
     try {
       await player.play();
       player.pause();
@@ -92,33 +116,23 @@ function AudioListItem({ room, audio }: { room: Room; audio: AudioData }) {
     }
   }, [player]);
 
-  const publishClick = useCallback(() => {
+  const onPlay = useCallback(() => {
     room.play(audio.id);
   }, [audio.id, room]);
 
-  const pause = useCallback(() => {
+  const onPause = useCallback(() => {
     if (isPlaying) {
       player.pause();
       player.currentTime = 0;
     }
   }, [isPlaying, player]);
 
-  return (
-    <Flex justifyContent="space-between" alignItems="center" h="10">
-      <Box>{audio.name}</Box>
-      {isRejected ? (
-        <Button size="sm" onClick={allow}>
-          再生を許可する
-        </Button>
-      ) : (
-        <IconButton
-          size="sm"
-          icon={isPlaying ? <FaPause /> : <FaPlay />}
-          aria-label="再生"
-          isLoading={!isPlayable}
-          onClick={isPlaying ? pause : publishClick}
-        />
-      )}
-    </Flex>
-  );
+  return {
+    isRejected,
+    onAllow,
+    isPlayable,
+    isPlaying,
+    onPlay,
+    onPause,
+  };
 }
