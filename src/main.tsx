d7066e2f291e870d5b34758c445b680dd8dfd7da
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2022-04-15 09:22:57 +0900

    feat: baseUrlを書き換え可能に修正

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,16 @@ import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { ApiClientProvider } from './api';
 
+const baseUrl =
+  (import.meta.env.VITE_BASE_URL as string | undefined) ||
+  'http://localhost:8080';
+
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <ColorModeScript />
-        <ApiClientProvider baseUrl="http://localhost:8080">
+        <ApiClientProvider baseUrl={baseUrl}>
           <App />
         </ApiClientProvider>
       </BrowserRouter>
